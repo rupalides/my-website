8fb54db42e1d5eed25e65a92115ab739fd785b19
Rupali <rdeshmukh@edgecast.com>
2015-02-11 10:25:04 -0800

    adding entire solution

---
+++ GitTraining1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTraining1.Startup))]
+namespace GitTraining1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
